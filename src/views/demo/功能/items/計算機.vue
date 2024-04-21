b99154123db75da47ebd93a46a987adbf0df5bc0
mizu <psdk55052004@gmail.com>
2024-04-21 23:59:55 +0800

    新增 功能/計算機: 計算機面板

--- src/views/demo/功能/items/計算機.vue
+++ src/views/demo/功能/items/計算機.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from 'vue'
-// console.log(eval((1+5)*2)) // eslint-disable-line
+console.log(eval((1+2)*3-(4+15))) // eslint-disable-line
 
 const data = ref([
   '', '修正', '清除', '倒退',
@@ -17,6 +17,26 @@ const data = ref([
 <div class="d-flex justify-content-center align-items-center"
      style="height:95vh;">
   <div class="計算機外殼 p-5">
+
+    <!-- 計算機面板 -->
+    <div class="text-end mb-10">
+      <!-- 公式區塊 -->
+      <div>
+        <p style="letter-spacing: 4px;">
+          (1+2)*3-(4+5)
+        </p>
+      </div>
+
+      <!-- 計算結果區塊 -->
+      <div>
+        <p style="font-size:45px;
+                  letter-spacing: 4px;
+                  line-height: 1.2;">
+          123456
+        </p>
+      </div>
+    </div>
+
     <!-- 計算機按鈕 -->
     <ul class="row row-cols-4 g-2 text-center">
       <template v-for="item in data" :key="item">
