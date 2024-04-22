ae082479a02e89577177c8e0ade6f337c5dc9138
Mizu <psdk55052004@gmail.com>
2024-04-22 13:30:25 +0800

    新增 圖片放大鏡: 滑鼠移動時改變放大鏡座標、滑鼠始終在放大鏡中央、且放大鏡不會超出圖片範圍

--- src/views/demo/功能/items/圖片放大鏡.vue
+++ src/views/demo/功能/items/圖片放大鏡.vue
@@ -4,8 +4,14 @@ import imgUrl from '@/assets/images/demo/功能/圖片放大鏡.jpg'
 
 // data
 const zoom = ref(2)
+const imgWidth = ref(300)
+const imgHeight = ref(300)
 const glassTop = ref(0)
 const glassLeft = ref(0)
+
+const root = document.documentElement
+root.style.setProperty('--img-width', `${imgWidth.value}px`)
+root.style.setProperty('--img-height', `${imgHeight.value}px`)
 
 // 設定圖片倍率
 watchEffect(() => {
@@ -14,37 +20,104 @@ watchEffect(() => {
   const el = document.querySelector('.zoom-img')
   if (el) el.style.setProperty('--zoom', zoomValue)
 })
+
+function mousemove (e) {
+  // 滑鼠在當前容器中的相對位置
+  console.log('mousemove', e)
+
+  // 計算出放大鏡寬高
+  const glassW = imgWidth.value / 4
+  const glassH = imgHeight.value / 4
+  // 將滑鼠在校正到放大鏡中間
+  const yMiddle = e.offsetY - (glassH / 2)
+  const xMiddle = e.offsetX - (glassW / 2)
+  console.log('xMiddle', xMiddle)
+  console.log('yMiddle', yMiddle)
+
+  // X邊界檢查 (放大鏡不得超出圖片邊界)
+  const leftPass = xMiddle > 0
+  const rightPass = xMiddle + glassW <= imgWidth.value
+  const isValiedX = leftPass && rightPass
+  // X軸 不超過圖片的左、右邊才賦予值
+  if (isValiedX) {
+    glassLeft.value = xMiddle
+  } else {
+    // 直接賦予邊界的值上去
+    if (!leftPass) {
+      // 將放大鏡 left 位置設定在圖片左邊
+      glassLeft.value = 0
+    } else if (!rightPass) {
+      // 將放大鏡 left 位置設定在圖片右邊
+      glassLeft.value = imgWidth.value - glassW
+    }
+  }
+
+  // Y邊界檢查 (放大鏡不得超出圖片邊界)
+  const topPass = yMiddle > 0
+  const bottomPass = yMiddle + glassH <= imgHeight.value
+  const isValiedY = topPass && bottomPass
+  // Y軸 不超過圖片的上、下面才賦予值
+  if (isValiedY) {
+    glassTop.value = yMiddle
+  } else {
+    if (!topPass) {
+      // 將放大鏡 left 位置設定在圖片上面
+      glassTop.value = 0
+    } else if (!bottomPass) {
+      // 將放大鏡 left 位置設定在圖片下面
+      glassTop.value = imgHeight.value - glassH
+    }
+  }
+  console.log('offsetX', e.offsetX)
+  console.log('offsetY', e.offsetY)
+}
 </script>
 
 <template>
   <div class="position-absolute-center-200 p-15">
     <div class="d-flex">
       <!-- 左側原始圖 -->
-      <div class="position-relative me-15">
+      <div class="position-relative me-15"
+           style="overflow: hidden;"
+           :style="[`max-width:${imgWidth}px;`,
+                    `max-height:${imgHeight}px;`]">
         <div>
           <img src="@/assets/images/demo/功能/圖片放大鏡.jpg"
                class="d-block"
                alt="圖片放大鏡"
-               width="300"
-               height="300">
+               :width="imgWidth"
+               :height="imgHeight">
         </div>
 
         <!-- 放大鏡 -->
-        <div class="glass position-absolute"
+        <div class="position-absolute d-block"
+             style="background-color: #ffffff56;"
              :style="[
-              `--top:${glassTop}px`,
-              `--left:${glassLeft}px`
-             ]">
+              `width: ${imgWidth / 4}px`,
+              `height: ${imgHeight / 4}px`,
+              `top:${glassTop}px`,
+              `left:${glassLeft}px`
+             ]"
+             >
+        </div>
+
+        <!-- 蓋在最上方的透明元素, 方便滑鼠事件取得 x, y -->
+        <div class="position-absolute top-0 bottom-0 start-0 end-0 z50"
+             @mousemove="mousemove">
         </div>
       </div>
+
       <!-- 右側放大圖 -->
-      <div style="max-width:300px;
-                  max-height:300px;
-                  overflow: hidden;">
+      <div style="overflow: hidden;"
+           :style="[`max-width:${imgWidth}px;`,
+                    `max-height:${imgHeight}px;`]">
         <div class="zoom-img"
-             :style="[`background-image:url(${imgUrl})`]">
+             :style="[`background-image:url(${imgUrl})`,
+                      `width:${zoom * imgWidth}px`,
+                      `height:${zoom * imgHeight}px`]">
         </div>
       </div>
+
     </div>
 
     <!-- 功能設定 -->
@@ -64,25 +137,10 @@ watchEffect(() => {
 </template>
 
 <style lang='scss' scope>
-:root {
-  --img-width: 300px;
-  --img-height: 300px;
-}
 .zoom-img {
-  --zoom: 2;
-  width: calc(var(--zoom) * var(--img-width));
-  height: calc(var(--zoom) * var(--img-height));
   display: block;
   background-size: cover;
   background-repeat: no-repeat;
   background-position: -300px 0;
-}
-.glass {
-  width: calc(var(--img-width) / 4);
-  height: calc(var(--img-height) / 4);
-  display: block;
-  top: var(--top);
-  left: var(--left);
-  background-color: #ffffff56;
 }
 </style>
