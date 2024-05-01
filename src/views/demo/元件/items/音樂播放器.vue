d153767eb0032dd5d66dc4ae7affd1cc4d02f31e
mizu <psdk55052004@gmail.com>
2024-05-01 21:14:07 +0800

    新增 音樂播放器: props 參數範例

--- src/views/demo/元件/items/音樂播放器.vue
+++ src/views/demo/元件/items/音樂播放器.vue
@@ -1,5 +1,46 @@
 <script setup>
 import Audio from '@/components/音樂播放器/Audio.vue'
+
+// const options = {
+// 撥放器基本外觀參數
+//   playerWidth: '',
+//   playerHeight: '',
+//   playerTextColor: '',
+//   playerBackgroundColor: '',
+
+// 播放進度條
+//   playerProgressWidth: '',
+//   playerProgressHeight: '',
+//   playerProgressBgColor: '',
+
+// 音量進度條
+//   volProgressWidth: '',
+//   volProgressHeight: '',
+//   volProgressColor: '',
+
+// 播放按鈕icon
+//   playIconUrl: '',
+//   playIconWidth: '',
+//   playIconHeight: '',
+
+// 暫停按鈕icon
+//   pauseIconUrl: '',
+//   pauseIconWidth: '',
+//   pauseIconHeight: '',
+
+// 有聲音icon
+//   volUpIconUrl: '',
+//   volUpIconWidth: '',
+//   volUpIconHeight: '',
+
+// 靜音icon
+//   volDownIconUrl: '',
+//   volDownIconWidth: '',
+//   volDownIconHeight: '',
+
+// 點擊播放器才取得音樂 url (非同步)
+//   async: true
+// }
 </script>
 
 <template>
