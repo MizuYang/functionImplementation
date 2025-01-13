1a2a4c2b526df255a7e93bb981a2eb0426607d1c
Mizu <psdk55052004@gmail.com>
2025-01-13 10:30:44 +0800

    WIP: 尚未完成的分支, 先提交

--- src/views/demo/功能/items/圖片放大鏡.vue
+++ src/views/demo/功能/items/圖片放大鏡.vue
@@ -8,11 +8,15 @@ const imgWidth = ref(300)
 const imgHeight = ref(300)
 const glassTop = ref(0)
 const glassLeft = ref(0)
+const bgPositionX = ref(0)
+const bgPositionY = ref(0)
+// overflowState: hidden | visible
+const overflowState = ref(localStorage.getItem('overflowState') || 'hidden')
 const throttleMouseMove = throttle(30, mousemove)
 
-const root = document.documentElement
-root.style.setProperty('--img-width', `${imgWidth.value}px`)
-root.style.setProperty('--img-height', `${imgHeight.value}px`)
+// const root = document.documentElement
+// root.style.setProperty('--img-width', `${imgWidth.value}px`)
+// root.style.setProperty('--img-height', `${imgHeight.value}px`)
 
 // 設定圖片倍率
 watchEffect(() => {
@@ -22,9 +26,14 @@ watchEffect(() => {
   if (el) el.style.setProperty('--zoom', zoomValue)
 })
 
+// 設定 overflow
+watchEffect(() => {
+  localStorage.setItem('overflowState', overflowState.value)
+})
+
 function mousemove (e) {
   // 滑鼠在當前容器中的相對位置
-  console.log('mousemove', e)
+  // console.log('mousemove', e)
 
   // 計算出放大鏡寬高
   const glassW = imgWidth.value / 4
@@ -32,8 +41,8 @@ function mousemove (e) {
   // 將滑鼠在校正到放大鏡中間
   const yMiddle = e.offsetY - (glassH / 2)
   const xMiddle = e.offsetX - (glassW / 2)
-  console.log('xMiddle', xMiddle)
-  console.log('yMiddle', yMiddle)
+  // console.log('xMiddle', xMiddle)
+  // console.log('yMiddle', yMiddle)
 
   // X邊界檢查 (放大鏡不得超出圖片邊界)
   const leftPass = xMiddle > 0
@@ -69,8 +78,23 @@ function mousemove (e) {
       glassTop.value = imgHeight.value - glassH
     }
   }
-  console.log('offsetX', e.offsetX)
-  console.log('offsetY', e.offsetY)
+  // console.log('glassLeft', glassLeft.value)
+  // console.log('glassTop', glassTop.value)
+  // 取得圖片放大座標
+  // bgPositionX.value = (glassLeft.value) * -1
+  // bgPositionY.value = (glassTop.value) * -1
+  console.log('e.offsetY', e.offsetY)
+  console.log('e.offsetX', e.offsetX)
+  // bgPositionX.value = (xMiddle * zoom.value) / 2 * -1
+  // bgPositionY.value = (yMiddle * zoom.value) / 2 * -1
+  bgPositionX.value = (e.offsetX * zoom.value) / 2 * -1
+  bgPositionY.value = (e.offsetY * zoom.value) / 2 * -1
+  // 1. 先算出放大倍率的圖片寬度
+  // bgPositionX.value = (imgWidth.value * zoom.value) * -1
+  // 2.計算出滑鼠在圖片上的相對位置
+
+  // e.offsetY
+  // e.offsetX
 }
 function throttle (delay, callback) {
   // 函式執行後在指定時間內不論呼叫函式多少次, 都不會執行
@@ -117,25 +141,55 @@ function throttle (delay, callback) {
 
         <!-- 蓋在最上方的透明元素, 方便滑鼠事件取得 x, y -->
         <div class="position-absolute top-0 bottom-0 start-0 end-0 z50"
-             @mousemove="throttleMouseMove">
+             @mousemove="throttleMouseMove"
+             @mouseenter="throttleMouseMove"
+             @mouseleave="throttleMouseMove">
         </div>
       </div>
 
       <!-- 右側放大圖 -->
-      <div style="overflow: hidden;"
+      <div class="position-relative"
            :style="[`max-width:${imgWidth}px;`,
-                    `max-height:${imgHeight}px;`]">
+                    `max-height:${imgHeight}px;`,
+                    `overflow: ${overflowState};`]">
         <div class="zoom-img"
              :style="[`background-image:url(${imgUrl})`,
-                      `width:${zoom * imgWidth}px`,
-                      `height:${zoom * imgHeight}px`]">
+                      `width:${zoom * imgWidth}px;`,
+                      `height:${zoom * imgHeight}px;`,
+                      `background-position-x: ${bgPositionX}px`,
+                      `background-position-y: ${bgPositionY}px`,]">
         </div>
-      </div>
 
+        <!-- overflow: visible 時的陰影 -->
+        <template v-if="overflowState==='visible'">
+          <div class="position-absolute top-0 bottom-0 start-0 end-0 z50"
+               style="background-color: rgb(83 0 138 / 37%)">
+          </div>
+        </template>
+      </div>
     </div>
 
     <!-- 功能設定 -->
     <div class="mt-15">
+      <p>
+        bgPositionX: {{ bgPositionX }} <br />
+        bgPositionY: {{ bgPositionY }} <br />
+      </p>
+      <!-- overflow -->
+      <div class='form-check d-flex align-items-center'>
+        <input class='form-check-input'
+               type='checkbox'
+               id='overflowState'
+               v-model="overflowState"
+               true-value="hidden"
+               false-value="visible">
+        <label class='form-check-label ms-3'
+               for='overflowState'>
+          隱藏溢出: {{ overflowState }}
+        </label>
+      </div>
+
+      <!-- 放大倍率 -->
       <div class='form-floating mb-3'>
         <input type='number'
                class='form-control'
@@ -143,7 +197,8 @@ function throttle (delay, callback) {
                placeholder='2'
                min="1"
                step="0.5"
-               v-model="zoom">
+               v-model="zoom"
+               style="width:200px;">
         <label for='floatingInput'>放大倍率</label>
       </div>
     </div>
@@ -155,6 +210,5 @@ function throttle (delay, callback) {
   display: block;
   background-size: cover;
   background-repeat: no-repeat;
-  background-position: -300px 0;
 }
 </style>
